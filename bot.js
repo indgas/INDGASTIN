f34de616d8a89651b3737fede6500686ab1b2c51
indgas <indgastin@gmail.com>
2024-08-29 14:47:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,25 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '7531617907:AAGMOyDoaCnIAhUIY698QwkejD_1NW4KAFE';
 const bot = new TelegramBot(token, { polling: true });
-
-
 bot.start((ctx) => {
     const refCode = ctx.message.text.split(' ')[1];
+    
+    if (refCode && refCode.startsWith('ref_')) {
+        const inviterId = refCode.split('_')[1];
+        const newUserId = ctx.from.id;
+        const username = ctx.from.username;
+
+        // Добавляем нового пользователя в базу данных
+        addPlayer(newUserId, username);
+
+        // Добавляем пригласившего как друга для нового пользователя
+        addFriend(inviterId, newUserId);
+
+        ctx.reply(`Добро пожаловать, ${username}! Вы приглашены пользователем с ID ${inviterId}.`);
+    } else {
+        // Обычный старт без реферального кода
+        ctx.reply('Добро пожаловать в игру!');
+    }
  {
         reply_markup: {
             inline_keyboard: [
@@ -14,5 +29,5 @@ bot.start((ctx) => {
                 }]
             ]
         }
-    };
-
+    }
+});
